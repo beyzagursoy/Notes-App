d629812ad58e3e4b87da1721b6f7c5e3a2202648
beyzagursoy <b.gursoy0112@gmail.com>
2024-08-30 00:41:45 +0300

    new

--- frontend/notesapp/vite.config.js
+++ frontend/notesapp/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: '/login',  // Projeyi başlattığınızda otomatik olarak /login açılır
+  },
 })
